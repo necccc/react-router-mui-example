719c6db3a0e1e8061e64e39c4eaf26926008dc47
Szabolcs Szabolcsi-Toth <szabolcs.szabolcsi-toth@binx.hu>
2026-01-05 16:20:39 +0100

    init

---
+++ app/routes/home.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import { Link as ReactRouterLink } from "react-router";
+import ProTip from "~/components/ProTip";
+import Copyright from "~/components/Copyright";
+import Paper from "@mui/material/Paper";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+
+export function meta() {
+  return [
+    { title: "Material UI - React Router example in TypeScript" },
+    {
+      name: "description",
+      content: "Welcome to Material UI - React Router example in TypeScript!",
+    },
+  ];
+}
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Material UI - React Router example in TypeScript
+        </Typography>
+        <Card sx={{ minWidth: 545 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image="/public/sergei-wing-RWElb9j7nlc-unsplash.jpg"
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Cat
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Cute fluffyness full of fluff
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" variant="contained">
+              Share
+            </Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </Container>
+  );
+}
